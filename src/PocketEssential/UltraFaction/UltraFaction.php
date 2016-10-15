ebe2733723254ff1b00d22b20b0940e00f9b846b
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2016-10-15 03:48:24 -0400

    Logs

--- src/PocketEssential/UltraFaction/UltraFaction.php
+++ src/PocketEssential/UltraFaction/UltraFaction.php
@@ -44,10 +44,17 @@ class UltraFaction extends PluginBase implements Listener
      */
     const PREFIX = TextFormat::YELLOW . "[" . TextFormat::AQUA . "Faction" . TextFormat::YELLOW . "]";
     public $lang = [];
+    public $data = null;
 
     public function onEnable()
     {
         $this->config = new Config($this->getDataFolder() . "Config.yml", Config::YAML);
+
+        $this->getLogger()->info("---------------------------------------");
+        $this->getLogger()->info("Using Language: ".$this->lang);
+        $this->getLogger()->info("Data-Provider: ".$data);
+        $this->getLogger()->info("|| Everything has been loaded ||| ");
+        $this->getLogger()->info("----------------------------------------");
     }
 
     public function onDisable()
@@ -77,76 +84,95 @@ class UltraFaction extends PluginBase implements Listener
         if (!file_exists('/Languages/')) {
             mkdir('/Languages', 0777, true);
         }
-        if (!(is_dir($this->getDataFolder() . "Languages/ $langType.yml"))){
-        $this->saveResource("Languages/$langType.yml");
+        if (!(is_dir($this->getDataFolder() . "Languages/ $langType.yml"))) {
+            $this->saveResource("Languages/$langType.yml");
             $langGet = (new Config($this->getDataFolder() . "Languages/" . $langType . ".yml", Config::YAML));
             $string = null;
             $this->getLanguageText($string);
             $this->lang = $langGet;
 
-            $this->getLogger()->info(TextFormat::DARK_BLUE . "Using".$langType." as the Language!");
-       }
-       }
+            $this->getLogger()->info(TextFormat::DARK_BLUE . "Using" . $langType . " as the Language!");
+        }
+    }
+
+    /*
+     *  Simple API to be used by (U-S )
+     */
+    public function getLanguageText($string)
+    {
+
+        if ($string == null) {
+            return "Please provide a string to translate!";
+
+        }
 
         /*
-         *  Simple API to be used by (U-S )
-         */
-       public function getLanguageText($string){
+         * Getting the type of text they want to translate!
+        */
 
-            if($string == null){
-                return "Please provide a string to translate!";
+        if ($string == "Created_A_Faction") {
+            return $this->lang->get("Created_A_Faction");
+        }
+        if ($string == "Faction_Already_Exist") {
+            return $this->lang->get("Faction_Already_Exist");
+        }
+        if ($string == "Faction_Too_Short") {
+            return $this->lang->get("Faction_Too_Short");
+        }
+        if ($string == "Faction_Too_Long") {
+            return $this->lang->get("Faction_Too_Long");
+        }
+        if ($string == "Already_In_Faction") {
+            return $this->lang->get("Already_In_Faction");
+        }
+        if ($string == "Faction_Cost_Reach") {
+            return $this->lang->get("ction_Cost_Reach");
+        }
+        if ($string == "Faction_Set_Description") {
+            return $this->lang->get("Faction_Set_Description");
+        }
+        if ($string == "Faction_Reset") {
+            return $this->lang->get("Faction_Reset");
+        }
+        if ($string == "Faction_Remove") {
+            return $this->lang->get("Faction_Remove");
+        }
+        if ($string == "Faction_Home_Teleport") {
+            return $this->lang->get("Faction_Home_Teleport");
+        }
+        if ($string == "Faction_No_Home") {
+            return $this->lang->get("Faction_No_Home");
+        }
+        if ($string == "Faction_No_Home_Set") {
+            return $this->lang->get("Faction_No_Home_Set");
+        }
+        if ($string == "Faction_Rename") {
+            return $this->lang->get("Faction_Rename");
+        }
+        if ($string == "Invited") {
+            return $this->lang->get("Invited");
+        }
+        if ($string == "No_Permission_To_Invite") {
+            return $this->lang->get("No_Permission_To_Invite");
+        }
+        if ($string == "No_Permission_To_Promote") {
+            return $this->lang->get("No_Permission_To_Promote");
+        }
+    }
 
-            }
-
-            /*
-             * Getting the type of text they want to translate!
-            */
-
-            if($string == "Created_A_Faction"){
-                return $this->lang->get("Created_A_Faction");
-            }
-           if($string == "Faction_Already_Exist"){
-               return $this->lang->get("Faction_Already_Exist");
-           }
-           if($string == "Faction_Too_Short"){
-               return $this->lang->get("Faction_Too_Short");
-           }
-           if($string == "Faction_Too_Long"){
-               return $this->lang->get("Faction_Too_Long");
-           }
-           if($string == "Already_In_Faction"){
-               return $this->lang->get("Already_In_Faction");
-           }
-           if($string == "Faction_Cost_Reach"){
-               return $this->lang->get("ction_Cost_Reach");
-           }
-           if($string == "Faction_Set_Description"){
-               return $this->lang->get("Faction_Set_Description");
-           }
-           if($string == "Faction_Reset"){
-               return $this->lang->get("Faction_Reset");
-           }
-           if($string == "Faction_Remove"){
-               return $this->lang->get("Faction_Remove");
-           }
-           if($string == "Faction_Home_Teleport"){
-               return $this->lang->get("Faction_Home_Teleport");
-           }
-           if($string == "Faction_No_Home"){
-               return $this->lang->get("Faction_No_Home");
-           }  if($string == "Faction_No_Home_Set"){
-               return $this->lang->get("Faction_No_Home_Set");
-           }
-           if($string == "Faction_Rename"){
-               return $this->lang->get("Faction_Rename");
-           }  if($string == "Invited"){
-               return $this->lang->get("Invited");
-           }
-           if($string == "No_Permission_To_Invite"){
-               return $this->lang->get("No_Permission_To_Invite");
-           }
-           if($string == "No_Permission_To_Promote"){
-               return $this->lang->get("No_Permission_To_Promote");
-           }
-       }
+    public function getDataProvider()
+    {
+        if ($this->config->get("data-provider") == "sqlite") {
+            $this->data = "sqlite";
+            return $this->data;
+        }
+        if ($this->config->get("data-provider") == "mysql") {
+            $this->data = "mysql";
+            return $this->data;
+        }
+        if ($this->config->get("data-provider") == "yaml") {
+            $this->data = "yaml";
+            return $this->data;
+        }
+    }
 }
