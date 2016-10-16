0987add1dd723a513cb61aa60a7b20b125852eab
Ankit <ankitmaharjankath@gmail.com>
2016-10-16 18:17:41 +0545

    Listener implementation

--- src/PocketEssential/UltraFaction/EventListener.php
+++ src/PocketEssential/UltraFaction/EventListener.php
@@ -2,8 +2,9 @@
 namespace PocketEssential\UltraFaction;
 
 use PocketEssential\UltraFaction\UltraFaction;
+use pocketmine\event\Listener;
 
-class EventListener {
+class EventListener implements Listener{
     public function __construct(UltraFaction $plugin){
         $this->plugin = $plugin;
     }
