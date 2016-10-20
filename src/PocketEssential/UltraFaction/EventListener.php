b7df9bb15bea7aa9ec4e564f9e1a87d44f6bbd62
Ankit <ankitmaharjankath@gmail.com>
2016-10-20 18:45:19 +0545

    Chat Event startup

--- src/PocketEssential/UltraFaction/EventListener.php
+++ src/PocketEssential/UltraFaction/EventListener.php
@@ -3,9 +3,14 @@ namespace PocketEssential\UltraFaction;
 
 use PocketEssential\UltraFaction\UltraFaction;
 use pocketmine\event\Listener;
+use pocektmine\event\player\PlayerChatEvent;
 
 class EventListener implements Listener{
     public function __construct(UltraFaction $plugin){
         $this->plugin = $plugin;
+    }
+    public function onChat(PlayerChatEvent $event){
+        $player = $event->getPlayer();
+        //TODO
     }
 }
