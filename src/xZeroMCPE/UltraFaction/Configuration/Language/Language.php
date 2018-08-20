5a951c1299c82fff5e1199b414425e3dd3167615
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2018-08-19 21:11:15 -0400

    Correctly load defaults
    Should load all defaults language!

--- src/xZeroMCPE/UltraFaction/Configuration/Language/Language.php
+++ src/xZeroMCPE/UltraFaction/Configuration/Language/Language.php
@@ -26,18 +26,21 @@ class Language
         $this->loadLanguage();
     }
 
-    public function loadLanguage(){
+    public function loadLanguage()
+    {
 
-        if(!file_exists(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/")){
+        if (!file_exists(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/")) {
             @mkdir(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages");
-            file_put_contents(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/" . "eng.json", file_get_contents(__DIR__ . "/Defaults/eng.json"));
+            foreach (glob(__DIR__ . "/Defaults/*") as $languages) {
+                file_put_contents(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/" . basename($languages), file_get_contents(__DIR__ . "/Defaults/" . basename($languages)));
+            }
         }
 
 
         $language = UltraFaction::getInstance()->getConfiguration()->getConfig()['Data']['Language'];
 
-        if(!file_exists(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/". $language . ".json")){
-            UltraFaction::getInstance()->getLogger()->error("[LANGUAGE] We couldn't find the language file corresponding to " . $language.". Please make sure {$language}.json exists");
+        if (!file_exists(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/" . $language . ".json")) {
+            UltraFaction::getInstance()->getLogger()->error("[LANGUAGE] We couldn't find the language file corresponding to " . $language . ". Please make sure {$language}.json exists");
         } else {
             $this->language = json_decode(file_get_contents(UltraFaction::getInstance()->getConfiguration()->getDataFolder() . "Languages/" . $language . ".json"), true);
             $this->workingLanguage = true;
@@ -48,8 +51,9 @@ class Language
      * @param string $type
      * @return string
      */
-    public function getLanguageValue(string $type) : string {
-        if(isset($this->language[$type])){
+    public function getLanguageValue(string $type): string
+    {
+        if (isset($this->language[$type])) {
             return $this->language[$type];
         } else {
             return "ERROR_LANGUAGE_NOT_FOUND [{$type}]";
@@ -60,8 +64,9 @@ class Language
      * @param string $type
      * @return array
      */
-    public function getLanguageValueArray(string $type) :array {
-        if(isset($this->language[$type])){
+    public function getLanguageValueArray(string $type): array
+    {
+        if (isset($this->language[$type])) {
             return $this->language[$type];
         } else {
             return ["ERROR_LANGUAGE_NOT_FOUND [{$type}]_ARRAY"];
